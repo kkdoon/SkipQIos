f3773cc17fe8cdc89a0f1bb10c2539fd5f87deab
kkdoon <kkdoon@gmail.com>
2016-07-28 04:20:44 -0400

    Initial Commit

---
+++ SkipQ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SkipQ
+//
+//  Created by Kanishk Karanawat on 7/28/16.
+//  Copyright © 2016 CMU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
